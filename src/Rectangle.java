ee2325d135a0276d5dac74c771b0fb9a4452e73c
Sukotoo <115269003+Sukotoo@users.noreply.github.com>
2023-01-31 09:23:20 -0800

    almost finished constructor overloading

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,13 +2,19 @@ import java.text.DecimalFormat;
 public class Rectangle { //make public for CS A
 
     //instance variables
-
+    int width;
+    int height;
     //default constructor (make public for CS A)
-
-
+    public void Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
     //overloaded constructor (with parameters)
+    public void Rectangle(int width, int height, double scale)
+    {
 
-
+    }
 
     //accessors
 
